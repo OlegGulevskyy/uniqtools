1724532963aa44b7a1d1bb78187824269c551026
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-05-27 17:28:50 +0200

    some responsiveness

--- src/screens/TimestampsDates/TimestampFromDateTime/TimestampFromDateTime.tsx
+++ src/screens/TimestampsDates/TimestampFromDateTime/TimestampFromDateTime.tsx
@@ -1,5 +1,5 @@
 import { CopyableBox } from '@/components/CopyableBox';
-import { Grid, Title, Button, Chips, Chip } from '@mantine/core';
+import { Grid, Title, Button, Chips, Chip, Group, Stack } from '@mantine/core';
 import { DatePicker, TimeInput } from '@mantine/dates';
 import { Calendar, Clock } from 'tabler-icons-react';
 import { useLogic } from './logic';
@@ -20,38 +20,50 @@ export const TimestampFromDateTime = () => {
   return (
     <>
       <Grid>
-        <Grid.Col span={4}>
-          <Title style={{ margin: '5px 0' }} order={6}>
-            Select date
-          </Title>
-          <DatePicker
-            icon={<Calendar size={16} />}
-            value={currentDate}
-            onChange={onDatePickerChange}
-            style={{ marginBottom: '10px' }}
-          />
-          <Button onClick={resetDate} variant="outline" compact>
-            Today
-          </Button>
+        <Grid.Col span={4} sm={6} md={4} xs={12}>
+          <Stack>
+            <Title style={{ margin: '5px 0' }} order={6}>
+              Select date
+            </Title>
+            <DatePicker
+              icon={<Calendar size={16} />}
+              value={currentDate}
+              onChange={onDatePickerChange}
+              style={{ marginBottom: '10px' }}
+            />
+            <Button onClick={resetDate} variant="outline" compact>
+              Today
+            </Button>
+          </Stack>
         </Grid.Col>
-        <Grid.Col span={4}>
-          <Title style={{ margin: '5px 0' }} order={6}>
-            Select time
-          </Title>
-          <TimeInput
-            clearable
-            icon={<Clock size={16} />}
-            value={currentTime}
-            onChange={setCurrentTime}
-            style={{ marginBottom: '10px' }}
-          />
-          <Button onClick={resetTime} variant="outline" compact>
-            Now
-          </Button>
+        <Grid.Col span={4} sm={6} md={4} xs={12}>
+          <Stack>
+            <Title style={{ margin: '5px 0' }} order={6}>
+              Select time
+            </Title>
+            <TimeInput
+              clearable
+              icon={<Clock size={16} />}
+              value={currentTime}
+              onChange={setCurrentTime}
+              style={{ marginBottom: '10px' }}
+            />
+            <Button onClick={resetTime} variant="outline" compact>
+              Now
+            </Button>
+          </Stack>
         </Grid.Col>
-      </Grid>
-      <Grid>
-        <Grid.Col span={8}>
+        <Grid.Col
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'end',
+          }}
+          span={4}
+          sm={6}
+          md={4}
+          xs={12}
+        >
           <CopyableBox enableTooltip displayValue={String(timestampResult)} />
           <Chips
             multiple={false}
