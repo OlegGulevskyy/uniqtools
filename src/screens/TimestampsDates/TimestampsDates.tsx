d2798f4aaf539c16d0f268838d451440cea34c5f
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-05-30 21:14:44 +0200

    add menu to the Timestamp screen

--- src/screens/TimestampsDates/TimestampsDates.tsx
+++ src/screens/TimestampsDates/TimestampsDates.tsx
@@ -1,4 +1,5 @@
-import { Title, Text, Paper, Divider } from '@mantine/core';
+import { Title, Text, Paper, Divider, Menu, Group } from '@mantine/core';
+import { IconDeviceFloppy, IconX } from '@tabler/icons';
 import { CurrentStats } from './CurrentStats';
 import { DateTimeFromTimestamp } from './DateTimeFromTimestamp';
 import { TimestampFromDateTime } from './TimestampFromDateTime';
@@ -6,12 +7,28 @@ import { TimestampFromDateTime } from './TimestampFromDateTime';
 export const Component = () => {
   return (
     <div>
-      <Title order={3}>
-        Parse and handle{' '}
-        <Text color="blue" inherit component="span">
-          timestamps and dates
-        </Text>
-      </Title>
+      <Group style={{ marginBottom: '20px' }}>
+        <Title order={3}>
+          Parse and handle{' '}
+          <Text color="blue" inherit component="span">
+            timestamps and dates
+          </Text>
+        </Title>
+        <Menu>
+          <Menu.Label>Settings</Menu.Label>
+          <Menu.Item
+            icon={<IconDeviceFloppy size={14} />}
+            rightSection={
+              <Text size="xs" color="dimmed">
+                ⌘S
+              </Text>
+            }
+          >
+            Save
+          </Menu.Item>
+          <Menu.Item icon={<IconX size={14} />}>Reset to default</Menu.Item>
+        </Menu>
+      </Group>
       <Paper shadow="xs" p="md">
         <Title order={5}>Current things</Title>
         <CurrentStats />
